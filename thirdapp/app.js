36b7f25d500916ed249a374fa64bf67d5a13355b
aakashDeveloper <ahanda205@gmail.com>
2019-06-08 04:47:26 +0100

    a

---
+++ thirdapp/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const app = express();
+const port = 9000;
+
+const menu =    [{'link':'/', 'name':'Home'}, 
+                {'link':'/movies', 'name':'Movies'}, 
+                {'link':'/artists', 'name':'Artits'}]
+
+const moviesRouter = require('./src/routes/moviesRoute')(menu);
+const ArtistRouter = require('./src/routes/artistsRoute')(menu);
+
+// logging middleware
+app.use(morgan('tiny'));
+
+//Static File Path
+app.use(express.static(__dirname+'/public'));
+// Html file path
+app.set('views', './src/views');
+// View Engine
+app.set('view engine', 'ejs');
+
+
+
+
+app.use('/movies',moviesRouter);
+app.use('/artists', ArtistRouter);
+app.get('/', (req,res) => {
+    res.render('index',{title:'Home Page',
+                        menu:menu})
+})
+
+
+app.listen(port, (err) => {
+    console.log(`${chalk.blue(`Server is running on port ${port}`)}`)
+})
---
+++ thirdapp/src/routes/artistsRoute.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+
+const ArtistRouter = express.Router();
+
+
+function router(menu) {
+    ArtistRouter.route('/')
+    .get((req,res) => {
+    res.render('artists',{title:'Artists Page',
+                        menu:menu})
+    })
+
+    ArtistRouter.route('/details')
+        .get((req,res) => {
+            res.render('details',{title:'Artists Details',
+                        menu:menu})
+    })
+
+    return ArtistRouter
+}            
+
+
+module.exports = router;
